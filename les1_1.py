b5989eff03302a37f3581ee984abbf9588208569
PrudnikovSergey <kanonir86rus@yandex.ru>
2020-12-10 21:35:36 +0300

    take files les1_1.py and les1_2.py from main to lesson1

---
+++ les1_1.py
@@ -0,0 +1,34 @@
+"""
+целочисленные int
+дробные числа float
+строки str
+булевые bool
+
+"""
+"""
+переменная пишется snake_case, начинается всегда со строчного символа или _
+"""
+'''
+тут
+комментарий
+на
+несколько
+'''
+
+
+var_integer1 = 2
+var_integer2 = 3
+
+variable = 1
+
+dogs_count = 3
+dog_name = "Dog"
+cat_name = 'Мурзик'
+worker_say = 'Михалыч, засунь ту "Штуковину" обратно доп символ\''
+
+# float
+profit_percent = 2.33  # комментарий в одну строку
+
+variable3 = variable + dogs_count
+
+print(variable3)
